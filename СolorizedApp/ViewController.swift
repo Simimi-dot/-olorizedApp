ef874601e1c7055a4c860cccd7ff4f9f6aa55ce5
Egor Astakhov <noise718@gmail.com>
2021-10-21 22:24:51 +0300

    Initial Commit

---
+++ СolorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  СolorizedApp
+//
+//  Created by Егор Астахов on 21.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
